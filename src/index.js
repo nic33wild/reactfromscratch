b7338b55b574d8a21eaecb54087e66cbc031cdb9
Nicola Trentini <nicola.trentini.wild@gmail.com>
2020-07-28 00:45:31 +0200

    Added store actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configuredStore } from "./store";
 import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={configuredStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
